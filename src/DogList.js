3e2104a8e361882c1133dd728be55e3e15e9939e
Evan Ishibashi <evan.ishibashi@gmail.com>
2023-10-02 15:23:54 -0700

    doglist made

--- src/DogList.js
+++ src/DogList.js
@@ -1,12 +1,34 @@
+
+
 function DogList({ dogList }) {
-  console.log(dogList)
+  console.log(dogList);
   return (
     <>
-    {dogList === "Loading"
-    ? <p>{dogList}</p>
-    : <p>THERE IS DOG LIST</p>}
+      {dogList === "Loading"
+        ? <p>{dogList}</p>
+        : <>
+          {dogList.map(d => {
+            return (
+              <div key={d.name}>
+
+              <img src={`${d.name}.jpg`} />
+              Name: {d.name} <br/>
+              Age: {d.age}
+              <ul>
+                {d.facts.map((fact, idx) =>
+                  <li key={idx}>{fact}</li>
+                )}
+              </ul>
+
+              </div>
+
+            )
+          })}
+        </>
+
+      }
     </>
-  )
+  );
 }
 
 export default DogList;
